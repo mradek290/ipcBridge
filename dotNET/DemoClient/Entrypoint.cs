efb9e479ca54f39dd1500c4514bd17b4bf2eeefd
Marcel Rädeker <Marcel Rdeker>
2022-07-04 17:19:50 +0200

    Prepared message exchange test

--- dotNET/DemoClient/Entrypoint.cs
+++ dotNET/DemoClient/Entrypoint.cs
@@ -8,7 +8,15 @@ namespace DemoClient
         static void Main(string[] args)
         {
             var connection = BridgeServer.Connect("mar");
+            Console.Write("Say something: ");
+            string input = Console.ReadLine();
 
+            var raw = System.Text.Encoding.ASCII.GetBytes(input);
+            connection.Write(raw);
+
+            connection.Signal();
+            connection.Dispose();
+            Console.WriteLine("Exiting");
         }
     }
 }
--- dotNET/DemoServer/Entrypoint.cs
+++ dotNET/DemoServer/Entrypoint.cs
@@ -9,7 +9,17 @@ namespace DemoServer
         {
             var server = new BridgeServer("mar");
             var connection = server.AwaitConnection();
-            
+            connection.Await();
+
+            var buffer = new byte[1000];
+            connection.Read(buffer);
+            string message = BitConverter.ToString(buffer);
+
+            Console.WriteLine(message);
+
+            server.Dispose();
+            connection.Dispose();
+            Console.WriteLine("Exiting");
         }
     }
 }
